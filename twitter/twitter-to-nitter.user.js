fa761b5b097bbfb5e5fe3a0c27a46ffc8aaeeff9
Mattias <lassekongo83@users.noreply.github.com>
2023-11-21 08:47:04 +0100

    Update twitter-to-nitter.user.js

--- twitter/twitter-to-nitter.user.js
+++ twitter/twitter-to-nitter.user.js
@@ -1,16 +1,11 @@
 // ==UserScript==
-// @name         Twitter to nitter
-// @namespace    null
-// @version      0.1
-// @description  Redirect twitter to nitter
-// @match        https://twitter.com/*
-// @run-at       document-start
+// @name              Redirect x.com to Nitter
+// @namespace         ViolentMonkey Scripts
+// @description       Redirect X to Nitter
+// @include           https://x.com/*
+// @include           https://twitter.com/*
+// @run-at            document-start
+// @grant             none
 // ==/UserScript==
 
-redirectToPage("https://twitter.com", "https://nitter.net");
-
-function redirectToPage(page1, page2) {
-  if(window.location.href.indexOf(page1) != -1) {
-    window.location.href = page2 + window.location.pathname;
-  }
-}
+window.location.replace("https://nitter.net" + window.location.pathname + window.location.search);
